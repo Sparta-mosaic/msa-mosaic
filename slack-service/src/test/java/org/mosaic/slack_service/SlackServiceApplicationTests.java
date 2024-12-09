0b8ea5740bbb45d6d582b324eb89e9e9ab7c46ce
Hut234 <doitdoitdoitdoitdo@gmail.com>
2024-12-09 15:59:57 +0900

    init

---
+++ slack-service/src/test/java/org/mosaic/slack_service/SlackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mosaic.slack_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
