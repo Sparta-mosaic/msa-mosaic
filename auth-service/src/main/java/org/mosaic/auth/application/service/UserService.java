ff513f44dfdd5a34b7dc39d3c76197c31ffb574c
DISNOTACAT <yeonjh915@gmail.com>
2024-12-13 03:23:44 +0900

    feat: User search 구

--- auth-service/src/main/java/org/mosaic/auth/application/service/UserService.java
+++ auth-service/src/main/java/org/mosaic/auth/application/service/UserService.java
@@ -1,12 +1,19 @@
 package org.mosaic.auth.application.service;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
 import lombok.RequiredArgsConstructor;
 import org.mosaic.auth.application.dtos.UserDto;
+import org.mosaic.auth.application.dtos.UserPageResponse;
 import org.mosaic.auth.application.dtos.UserResponse;
 import org.mosaic.auth.domain.entity.user.User;
 import org.mosaic.auth.domain.repository.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.mosaic.auth.domain.entity.user.QUser.user;
 
 @Service
 @RequiredArgsConstructor
@@ -36,5 +43,13 @@ public class UserService {
     return UserResponse.of(user);
   }
 
+  @Transactional(readOnly = true)
+  public UserPageResponse findAllByQueryDslPaging(Predicate predicate, Pageable pageable) {
 
+    BooleanBuilder booleanBuilder = new BooleanBuilder(predicate);
+    booleanBuilder.and(user.isPublic.eq(true));
+    Page<User> userEntityPage = userRepository.findAll(booleanBuilder, pageable);
+    return UserPageResponse.of(userEntityPage);
+
+  }
 }
