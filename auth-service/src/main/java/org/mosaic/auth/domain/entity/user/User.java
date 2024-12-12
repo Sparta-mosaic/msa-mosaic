9cac583327514f607f8238b361e055468ddf8cd4
DISNOTACAY <yeonjh915@gmail.com>
2024-12-12 10:56:28 +0900

    rename: merge patch branch

--- auth-service/src/main/java/org/mosaic/auth/domain/entity/user/User.java
+++ auth-service/src/main/java/org/mosaic/auth/domain/entity/user/User.java
@@ -11,7 +11,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.Table;
-import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
