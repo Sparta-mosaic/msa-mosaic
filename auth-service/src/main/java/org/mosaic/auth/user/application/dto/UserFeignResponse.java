53982c55106535771cda0c16747a4a72b29420a4
DISNOTACAT <yeonjh915@gmail.com>
2024-12-16 03:04:08 +0900

    feat: auth-service 내 feignClient 를 활용하여 사용자정보를 반환하는 feignController 추가

--- auth-service/src/main/java/org/mosaic/auth/user/application/dto/UserFeignResponse.java
+++ auth-service/src/main/java/org/mosaic/auth/user/application/dto/UserFeignResponse.java
@@ -1,22 +1,20 @@
 package org.mosaic.auth.user.application.dto;
 
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
-import org.mosaic.auth.user.domain.entity.user.UserRole;
 import org.mosaic.auth.user.domain.entity.user.User;
 
 @Getter
-@Builder(access = AccessLevel.PRIVATE)
-public class UserResponse {
+@Builder
+public class UserFeignResponse {
 
   private Long userId;
   private String username;
   private String role;
   private String slackEmail;
 
-  public static UserResponse of(User user){
-    return UserResponse.builder()
+  public static UserFeignResponse of(User user){
+    return UserFeignResponse.builder()
         .userId(user.getUserId())
         .username(user.getUsername())
         .role(user.getRole().toString())
---
+++ auth-service/src/main/java/org/mosaic/auth/user/presentations/controller/UserFeignController.java
@@ -0,0 +1,29 @@
+package org.mosaic.auth.user.presentations.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.mosaic.auth.user.application.dto.UserFeignResponse;
+import org.mosaic.auth.user.application.service.UserQueryService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("api/v1/feign/user")
+@RequiredArgsConstructor
+@Slf4j
+public class UserFeignController {
+
+    private final UserQueryService userQueryService;
+
+    @GetMapping("/{userId}")
+    public UserFeignResponse getUser(
+        @PathVariable Long userId) {
+        log.info("Get User By FeignClient Success!!");
+        return userQueryService.getFeignUserResponse(userId);
+
+    }
+
+}
