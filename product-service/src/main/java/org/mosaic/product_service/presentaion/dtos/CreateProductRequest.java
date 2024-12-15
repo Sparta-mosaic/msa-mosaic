26fe6ae136f17df7a369c4910ccc9c570e89768c
lowgiant <lowgiant@gmail.com>
2024-12-15 12:56:14 +0900

    refactor: simplify regular expression in @Pattern annotation
    - 정규 표현식에서 [0-9]를 \d로 변경하여 간결하게 표현
    - 코드 가독성과 유지보수성을 향상
    - 정적 분석 도구의 권장 사항을 준수

--- product-service/src/main/java/org/mosaic/product_service/presentaion/dtos/CreateProductRequest.java
+++ product-service/src/main/java/org/mosaic/product_service/presentaion/dtos/CreateProductRequest.java
@@ -1,36 +1,35 @@
 package org.mosaic.product_service.presentaion.dtos;
 
-import org.mosaic.product_service.application.dtos.CreateProductDto;
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.mosaic.product_service.application.dtos.CreateProductDto;
 
 @Getter
 @AllArgsConstructor
 public class CreateProductRequest {
-	private String companyId;
-	private String productHubId;
-	@NotBlank
-	private String productName;
-	@NotBlank
-	@Pattern(regexp = "^[0-9]+$", message = "숫자만 입력 가능합니다")
-	private String productPrice;
-	@NotBlank
-	private String productDescription;
-	@NotBlank
-	@Pattern(regexp = "^[0-9]+$", message = "숫자만 입력 가능합니다")
-	private String stockQuantity;
+  private String companyId;
+  private String productHubId;
+  @NotBlank private String productName;
 
-	public CreateProductDto toDto(){
-		return CreateProductDto.create(
-			this.companyId,
-			this.productHubId,
-			this.productName,
-			this.productPrice,
-			this.productDescription,
-			this.stockQuantity
-		);
-	}
+  @NotBlank
+  @Pattern(regexp = "^\\d+$", message = "숫자만 입력 가능합니다")
+  private String productPrice;
+
+  @NotBlank private String productDescription;
+
+  @NotBlank
+  @Pattern(regexp = "^\\d+$", message = "숫자만 입력 가능합니다")
+  private String stockQuantity;
+
+  public CreateProductDto toDto() {
+    return CreateProductDto.create(
+        this.companyId,
+        this.productHubId,
+        this.productName,
+        this.productPrice,
+        this.productDescription,
+        this.stockQuantity);
+  }
 }
