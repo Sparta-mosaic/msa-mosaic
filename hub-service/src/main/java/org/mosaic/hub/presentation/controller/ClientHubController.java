594eeb79c5babb02aa7f295037280a1c31c8e32f
Hut234 <doitdoitdoitdoitdo@gmail.com>
2024-12-17 17:03:27 +0900

    [#5] refactor: Add Internal Hub Read Endpoint

--- hub-service/src/main/java/org/mosaic/hub/presentation/controller/ClientHubController.java
+++ hub-service/src/main/java/org/mosaic/hub/presentation/controller/ClientHubController.java
@@ -4,10 +4,12 @@ import static org.mosaic.hub.libs.util.ApiResponseUtils.ok;
 
 import lombok.RequiredArgsConstructor;
 import org.mosaic.hub.application.dtos.GetHubPathResponse;
+import org.mosaic.hub.application.dtos.HubResponse;
 import org.mosaic.hub.application.service.HubQueryService;
 import org.mosaic.hub.libs.util.ApiResponseUtils.CommonResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ClientHubController {
 
   private final HubQueryService hubQueryService;
+
+  @GetMapping("/api/v1/internal/hubs/{hubUuid}")
+  public ResponseEntity<CommonResponse<HubResponse>> getHub(
+      @PathVariable String hubUuid) {
+    return ok(hubQueryService.getHub(hubUuid));
+  }
 
   @GetMapping("/api/v1/internal/hub-paths")
   public ResponseEntity<CommonResponse<GetHubPathResponse>> getHubPath(
