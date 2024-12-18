5aea7934b8291a279620a088334ca70622255b8b
lowgiant <lowgiant@gmail.com>
2024-12-18 10:22:07 +0900

    feat: 설정파일 누락

---
+++ delivery-service/src/test/java/org/mosaic/delivery_service/DeliveryServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package org.mosaic.delivery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
