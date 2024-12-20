fb01ed2b2c19127e52a3ed3871d1c3ee31659dd3
DISNOTACAT <yeonjh915@gmail.com>
2024-12-20 17:14:09 +0900

    refactor: slack-service 의 global exception, apiUtil   코드스타일 리팩토링

--- slack-service/src/main/java/org/mosaic/slack/libs/exception/GlobalExceptionHandler.java
+++ slack-service/src/main/java/org/mosaic/slack/libs/exception/GlobalExceptionHandler.java
@@ -1,10 +1,7 @@
 package org.mosaic.slack.libs.exception;
 
-import static org.mosaic.slack.libs.util.ApiResponseUtil.error;
-
-import org.mosaic.slack.libs.util.ApiResponseUtil.ApiError;
-import org.mosaic.slack.libs.util.ApiResponseUtil.ApiResult;
-import org.springframework.http.HttpHeaders;
+import org.mosaic.slack.libs.util.ApiResponseUtils;
+import org.mosaic.slack.libs.util.ApiResponseUtils.CommonResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,7 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(CustomException.class)
-	public ResponseEntity<ApiResult<ApiError>> handleCustomException(CustomException e) {
-		return new ResponseEntity<>(error(e.getMessage()), new HttpHeaders(), e.getStatus());
+	public ResponseEntity<CommonResponse<String>> handleCustomException(ExceptionStatus e) {
+		return ApiResponseUtils.failed(e);
 	}
 }
